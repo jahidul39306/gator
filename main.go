cf3e629f3ea66988838622fc3ff8e0cc8d41cf00
jahidul39306 <jahidulislamananto@gmail.com>
2026-03-11 14:42:11 +0100

    added login command

--- main.go
+++ main.go
@@ -2,23 +2,65 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/jahidul39306/gator/internal/config"
 )
 
+type state struct {
+	cfg *config.Config
+}
+
+type command struct {
+	name      string
+	arguments []string
+}
+
+type commands struct {
+	commandNames map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	if handler, exists := c.commandNames[cmd.name]; exists {
+		return handler(s, cmd)
+	}
+	return fmt.Errorf("unknown command: %s", cmd.name)
+}
+
+func (c *commands) register(name string, handler func(*state, command) error) {
+	c.commandNames[name] = handler
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.arguments) < 1 {
+		return fmt.Errorf("username is required")
+	}
+	username := cmd.arguments[0]
+	err := s.cfg.SetUser(username)
+	if err != nil {
+		return fmt.Errorf("failed to set user: %w", err)
+	}
+	fmt.Printf("User '%s' has been set\n", username)
+	return nil
+}
+
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
-		fmt.Println("Error reading config:", err)
-		return
+		log.Fatal(err)
 	}
-	fmt.Println("DB URL:", cfg.DBURL)
-	fmt.Println("Current User Name:", cfg.CurrentUserName)
+	sta := state{cfg: cfg}
+	cmds := commands{commandNames: make(map[string]func(*state, command) error)}
+	cmds.register("login", handlerLogin)
 
-	err = cfg.SetUser("Jahidul")
-	if err != nil {
-		fmt.Println("Error setting user:", err)
-		return
+	args := os.Args[1:]
+	if len(args) == 0 {
+		log.Fatal(fmt.Errorf("not enough arguments were provided"))
 	}
-	fmt.Println("Updated Current User Name:", cfg.CurrentUserName)
+	cmd := command{name: args[0], arguments: args[1:]}
+	err = cmds.run(&sta, cmd)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
