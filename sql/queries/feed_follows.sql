c2538b50c4b872e38b5526792282a0de0dbea5aa
jahidul39306 <jahidulislamananto@gmail.com>
2026-03-17 11:03:19 +0100

    query mistake fixed

--- sql/queries/feed_follows.sql
+++ sql/queries/feed_follows.sql
@@ -24,6 +24,6 @@ SELECT
     users.name AS user_name, 
     feeds.name AS feed_name 
 FROM feed_follows
-    INNER JOIN users ON feeds.user_id = users.id
+    INNER JOIN users ON feed_follows.user_id = users.id
     INNER JOIN feeds ON feed_follows.feed_id = feeds.id
 WHERE feed_follows.user_id = $1;
